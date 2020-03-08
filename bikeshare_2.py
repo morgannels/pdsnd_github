290300f0f319a3bfe29b012a49b58b6568282570
Morgan Sandquist <morgannels@gmail.com>
2020-03-08 15:33:07 -0400

    refactor: Better message to continue analysis
    Upon completing analysis for a given city, it's clearer to ask
    if user would like analyze another than restart

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -191,7 +191,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to examine another city? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
